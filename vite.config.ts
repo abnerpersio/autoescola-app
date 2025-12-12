af51983c3472b1636f8e96138a90f32d905fbb12
Abner Persio <binhopersio@gmail.com>
2025-12-12 16:42:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      base: process.env.GITHUB_ACTIONS
+        ? '/autoescola-app/' 
+        : '/',
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
